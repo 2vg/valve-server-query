2c77606667680067e26c0bf0a5e4ed3a0522bdf7
momf <mail@ni-vg.com>
2019-11-06 06:57:38 +0900

    move

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,1 +1,28 @@
-pub mod parser;
+extern crate serde_json;
+
+pub use serde_json::{json, Value};
+
+pub fn parse_response(response: &Vec<u8>) -> std::result::Result<Value, String> {
+    if response[0..3] != [0xff, 0xff, 0xff, 0xff] {
+        return Err(error_helper_for_parse_response("0..3", &response[0..3]))
+    }
+
+    if response[4] != 0x49 {
+        return Err(error_helper_for_parse_response("4", &[response[4]]))
+    }
+
+    // WIP
+
+    let ret_json = json!("{}");
+
+    Ok(json!("{}"))
+}
+
+pub fn bytes_to_char_and_map(bytes: &[u8]) -> String {
+    bytes.iter().map(|&s| format!("{}, ", s.to_string())).collect::<String>()
+}
+
+fn error_helper_for_parse_response(position: &'static str, buffer: &[u8]) -> String {
+    format!("response invalid -> {}: {}", position, bytes_to_char_and_map(buffer))
+}
+
--- src/query/mod.rs
+++ src/query/mod.rs
@@ -1,1 +1,52 @@
-pub mod query;
+extern crate async_std;
+
+use async_std::io;
+use async_std::net::UdpSocket;
+use async_std::task;
+
+// reference: https://developer.valvesoftware.com/wiki/Server_queries
+
+const SPECIAL_CHARA_VEC: &'static [u8] = &[0xff, 0xff, 0xff, 0xff];
+const PAYLOAD: &'static [u8] = "Source Engine Query".as_bytes();
+
+pub fn info_query<'a>(host: &'a str, port: &'a str) -> io::Result<Vec<u8>> {
+    let address = format!("{}:{}", host, port);
+
+    return task::block_on(async {
+        let socket = UdpSocket::bind("0.0.0.0:12345").await?;
+
+        let query_type = 0x54; // 'T'
+
+        let query = build_query(query_type, None as Option<&[u8]>);
+
+        socket.send_to(&query, &address).await?;
+
+        let mut buf = vec![0u8; 1024];
+        let (n, _) = socket.recv_from(&mut buf).await?;
+
+        Ok(Vec::from(&buf[..n]))
+    });
+}
+
+pub fn build_query(query_type: u8, challenge_token: Option<&[u8]>) -> Vec<u8> {
+    let mut query = Vec::new();
+
+    query.extend_from_slice(&SPECIAL_CHARA_VEC);
+    query.push(query_type);
+
+    match challenge_token {
+        Some(token) => {
+            query.extend_from_slice(token);
+        },
+        None => {
+            if query_type == 0x55 {
+                query.extend_from_slice(&SPECIAL_CHARA_VEC);
+            } else {
+                query.extend_from_slice(&PAYLOAD);
+                query.push(0x00);
+            }
+        }
+    };
+
+    return query;
+}
